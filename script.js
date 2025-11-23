719ab0877987fa9cd71fbcf182ed8dfdbee386f1
Dinuka Liyanage <infodinukalk@gmail.com>
2025-11-24 01:45:24 +0530

    feat(quiz): load questions from external JSON file

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-//question array
-const questions = [
-  {
-    question: "1. What does HTML stand For?",
-    options: [
-      "Hyper Text Markup Language",
-      "Hyperlinks and Text Markup Language",
-      "Home Tool Markup Language",
-      "Markup Hyper Text Language",
-    ],
-    answer: "Hyper Text Markup Language",
-  },
-];
+fetch("questions.json")
+  .then(repoonse => repoonse.json())
+  .then(data=>{
+    questions = data;
+    console.log(data);
+    
+  }).catch(error => console.error(error));
 
 document.getElementById("startBtn").addEventListener("click", function () {
   let username = document.getElementById("userName").value;
@@ -19,6 +13,7 @@ document.getElementById("startBtn").addEventListener("click", function () {
   document.querySelector(".loginPage").style.display = "none";
   document.querySelector(".quizzPage").style.display = "block";
   setTimerOn();
+  loadQuestions();
 });
 
 function setTimerOn() {
@@ -28,3 +23,41 @@ function setTimerOn() {
     time--;
   }, 1000);
 }
+
+
+function loadQuestions(){
+  document.querySelector('.quizzContainer').innerHTML+=`
+       <div class="questionText">
+            <p>${questions[1].question}</p>
+          </div>
+          
+          <div class="quizes">
+            <div class="questions">
+              <label>
+                <input type="radio" name="question1" />
+                ${questions[1].options[0]}
+              </label>
+            </div>
+            <div class="questions">
+              <label>
+                <input type="radio" name="question1" />
+                ${questions[1].options[1]}
+              </label>
+            </div>
+            <div class="questions">
+              <label>
+                <input type="radio" name="question1" />
+                ${questions[1].options[2]}
+              </label>
+            </div>
+            <div class="questions">
+              <label>
+                <input type="radio" name="question1" />
+                ${questions[1].options[3]}
+              </label>
+            </div>
+          </div>
+  `
+}
+
+let questions;
